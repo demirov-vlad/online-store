e3c563d30f4684dba187e62c35f2e519bc5133f0
vdemirov <demirov.vlad@gmail.com>
2023-09-04 23:01:55 +0800

    Search functionality added and pagination, replacing of props drilling with REACT CONTEXT

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,5 +1,41 @@
 import React from "react";
 import styles from "./Search.module.scss";
+import { SearchContext } from "../../App";
+
 export const Search = () => {
-  return <input className={styles.root} placeholder="Pizza search..." />;
+  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  return (
+    <div className={styles.root}>
+      <svg
+        className={styles.icon}
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title />
+        <g id="search">
+          <path d="M29.71,28.29l-6.5-6.5-.07,0a12,12,0,1,0-1.39,1.39s0,.05,0,.07l6.5,6.5a1,1,0,0,0,1.42,0A1,1,0,0,0,29.71,28.29ZM14,24A10,10,0,1,1,24,14,10,10,0,0,1,14,24Z" />
+        </g>
+      </svg>
+      <input
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+        className={styles.input}
+        placeholder="Pizza search..."
+      />
+      {searchValue && (
+        <svg
+          onClick={() => setSearchValue("")}
+          className={styles.clearIcon}
+          version="1.1"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="grid_system" />
+          <g id="_icons">
+            <path d="M5.3,18.7C5.5,18.9,5.7,19,6,19s0.5-0.1,0.7-0.3l5.3-5.3l5.3,5.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3   c0.4-0.4,0.4-1,0-1.4L13.4,12l5.3-5.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L6.7,5.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4   l5.3,5.3l-5.3,5.3C4.9,17.7,4.9,18.3,5.3,18.7z" />
+          </g>
+        </svg>
+      )}
+    </div>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,15 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import PizzaBlock from "../components/PizzaBlock";
+import Pagination from "../components/Pagination";
+import { SearchContext } from "../App";
 
 export const Home = () => {
+  const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({
     name: "popular",
     sortProp: "rating",
@@ -20,9 +24,10 @@ export const Home = () => {
     const sortBy = sortType.sortProp.replace("-", "");
     const order = sortType.sortProp.includes("-") ? "asc" : "desc";
     const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const search = searchValue ? `search=${searchValue}` : "";
 
     fetch(
-      `https://64eee824219b3e2873c39a29.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://64eee824219b3e2873c39a29.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&${search}`,
     )
       .then((response) => response.json())
       .then((array) => {
@@ -30,7 +35,14 @@ export const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items.map((object) => (
+    <PizzaBlock key={object.id} {...object} />
+  ));
+  const skeletons = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
 
   return (
     <div className="container">
@@ -45,11 +57,8 @@ export const Home = () => {
         />
       </div>
       <h2 className="content__title">All pizzas</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((object) => <PizzaBlock key={object.id} {...object} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
